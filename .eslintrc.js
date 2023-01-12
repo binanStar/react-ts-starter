8aa4a5988b0a7e9a9bb086310a5841003934577c
meta-crawler <williamlei0906@gmail.com>
2023-01-11 22:10:36 -0500

    create react typescript project

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "error",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "react/jsx-filename-extension": ["error", { extensions: [".tsx"] }],
+  },
+  overrides: [
+    {
+      files: [
+        "**/*.test.js",
+        "**/*.test.jsx",
+        "**/*.test.tsx",
+        "**/*.spec.js",
+        "**/*.spec.jsx",
+        "**/*.spec.tsx",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
